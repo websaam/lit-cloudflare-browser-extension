13c924d9435f4678adb579d38bbbe4dc4f0faf50
Anson Cheung <anson@Ansons-MacBook-Pro.local>
2021-12-08 03:15:30 +0000

    add: lit-unlock.js

---
+++ lit-unlock.js
@@ -0,0 +1,54 @@
+
+// ----- helper
+//
+// convert data URI to blob
+// @param { String } dataURI
+// @return { Blob } blob object
+//
+function dataURItoBlob(dataURI) {
+    var byteString = atob(dataURI.split(',')[1]);
+    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0]
+    var ab = new ArrayBuffer(byteString.length);
+    var ia = new Uint8Array(ab);
+    for (var i = 0; i < byteString.length; i++) {
+        ia[i] = byteString.charCodeAt(i);
+    }
+    var blob = new Blob([ab], {type: mimeString});
+    return blob;
+}
+
+buf2hex = (buffer) => [...new Uint8Array(buffer)]
+        .map((x) => x.toString(16).padStart(2, "0"))
+        .join("");
+
+// ----- unlock
+async function unlock(e){
+    var data = JSON.parse(atob(e.getAttribute('data-lit')));
+
+    const accessControlConditions = JSON.parse(atob(data['accessControlConditions']));
+    // console.log(accessControlConditions)
+    const encryptedZip = dataURItoBlob(data['encryptedZip']);
+    const toDecrypt = buf2hex(new Uint8Array(atob(data['encryptedSymmetricKey']).split(',').map((x) => parseInt(x))));
+    const chain = accessControlConditions[0].chain;
+    const authSig = await LitJsSdk.checkAndSignAuthMessage({chain: chain});
+    
+    const decryptedSymmetricKey = await window.litNodeClient.getEncryptionKey({
+        accessControlConditions,
+        toDecrypt,
+        chain,
+        authSig
+    });
+
+    const decryptedFiles = await LitJsSdk.decryptZip(encryptedZip, decryptedSymmetricKey);
+    const decryptedString = await decryptedFiles["string.txt"].async("text"); // video id
+
+    const url = `https://iframe.videodelivery.net/${decryptedString}`;
+    e.src = url;
+}
+
+// mounted
+(() => {
+    [...document.getElementsByClassName('btn-lit-video-unlock')].forEach((btn) => {
+        btn.addEventListener('click', (e) => unlock(e.target.parentElement.querySelector('iframe')));
+    });
+})();
